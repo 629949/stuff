1961263b8d772f1957f509b2592d9232378f3d34
629949 <46451202+629949@users.noreply.github.com>
2019-02-21 17:05:12 +0300

    Add files via upload

---
+++ AGE.py
@@ -0,0 +1,9 @@
+age= int(input("How old are you:"))
+
+if age<= 17:
+    print('your a minor')
+
+elif age>= 18 and age<= 36:
+        print('you are a youth')
+else:
+            print('you are an elder')
---
+++ numbers.py
@@ -0,0 +1,11 @@
+
+def squre():
+    x = 0
+    dict={}
+    for x in range(x,15):
+        sqrt = x**2
+        dict[x]=sqrt
+
+        print(dict)
+
+squre()
